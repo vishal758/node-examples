b1818229d6a84302433e57649d39b21a7d410a85
vishal758 <vishalchaudhary758@gmail.com>
2018-03-08 10:42:45 +0530

    node, callbacks and error handling

--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,28 @@
-exports.perimeter =  (x, y) => (2*(x+y));
+// The reason why I'm using the setTimeout is to simulate
+//  a delay before the callback comes in from the other side.
+ 
+module.exports = (x,y,callback) => {
+	if(x<=0 || y<=0){
+		setTimeout(() => 
+			callback(new Error("rectangle dimnensions should be greater than zero:  l = "
+               + x + ", and b = " + y),null) , 2000);
+		
+	}
+	else {
+		setTimeout(() => 
+			callback(null,
+			{
+				perimeter: () => (2*(x+y)),
+				area: () => (x*y)
 
-exports.area = (x, y) => (x*y);
+			}) ,
+		 2000);
+	}
+} 
+
+
+
+
+
+//perimeter =  (x, y) => (2*(x+y));
+//area = (x, y) => (x*y);
